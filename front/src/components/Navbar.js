8f5046de1744675c618feae70ab2d799dcb698ae
Your Name <you@example.com>
2024-11-22 15:49:02 +0800

    Back

--- front/src/components/Navbar.js
+++ front/src/components/Navbar.js
@@ -2,13 +2,13 @@ import {Link} from "react-router-dom";
 
 const Navbar = () => {
     return (
-        <nav>
-            <div>
-               <Link to="/">CRUD</Link>
+        <nav className ="navbar navbar-expand-lg bg-body-tertiary mb-5">
+            <div className="container">
+               <Link className="navbar-brand" to="/">CRUD</Link>
                <div>
-                <ul>
-                  <li>
-                    <Link to="/add">Add User</Link>
+                <ul classNamae="navbar-nav me-auto mb-2 mb-lg-0">
+                  <li className="nav-item">
+                    <Link className="nav-link" to="/add">Add User</Link>
                   </li>  
                 </ul>
                </div>            
